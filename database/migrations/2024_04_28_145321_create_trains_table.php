033d61e12103812bdd0f8f44adc4fead9195e367
agostinoluca <agostino.luca89@gmail.com>
2024-04-28 17:30:48 +0200

    make rollback add new columns to trains table make migrate

--- database/migrations/2024_04_28_145321_create_trains_table.php
+++ database/migrations/2024_04_28_145321_create_trains_table.php
@@ -11,8 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // COLUMNS:
+        // company
+        // departure_station
+        // arrival_station
+        // departure_time
+        // arrival_time
+        // train_code
+        // number_of_carriages
+        // on_time
+        // cancelled
+
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company')->nullable();
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time')->nullable();
+            $table->time('arrival_time')->nullable();
+            $table->string('train_code');
+            $table->unsignedInteger('number_of_carriages')->nullable();
+            $table->boolean('on_time')->nullable();
+            $table->boolean('cancelled')->nullable();
             $table->timestamps();
         });
     }
